72c0111bb37043733aea4b57537f384d756310bc
Joe Block <jpb@unixorn.net>
2015-11-13 15:50:28 -0800

    Fix up setup.py

--- setup.py
+++ setup.py
@@ -6,18 +6,16 @@
 from setuptools import setup, find_packages
 
 name = "actadiurna"
-requirements = map(str.strip, open("requirements.txt").readlines())
 
 setup(
   name = name,
-  version = "0.0.1",
+  version = "0.0.3",
   description = "Acta Diurna is a tool for updating /etc/motd",
   author = "Joe Block",
   author_email = "jpb@unixorn.net",
   url = "https://github.com/unixorn/acta-diurna",
-  download_url = "https://github.com/unixorn/acta-diurna/tarball/0.0.1",
+  download_url = "https://github.com/unixorn/acta-diurna/tarball/0.0.3",
   packages = find_packages(),
-  install_requires = requirements,
   entry_points = {
     "console_scripts": [
       "acta-diurna = %s.cli:actaMain" % name
